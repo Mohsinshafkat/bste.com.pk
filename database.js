e8a42385e259cd0fdb77e424cdfc86f6ba47c8bc
Mohsinshafkat <Mohsinshafkat@accountant.com>
2026-04-23 19:41:55 +0500

    Create database.js

---
+++ database.js
@@ -0,0 +1,77 @@
+const studentDB = [
+    {
+        roll: "90605",
+        year: "2026",
+        name: "SHAFQAT ALI",
+        father: "SHOUKAT ALI",
+        reg: "BSTE/TEC/A261-9742",
+        course: "DAE (MECHANICAL)",
+        total: "3450",
+        obt: "2599",
+        per: "75.33%",
+        grade: "A"
+    },
+    {
+        roll: "90606",
+        year: "2025",
+        name: "MOHSIN KHAN",
+        father: "AHMED KHAN",
+        reg: "BSTE/TEC/A261-9743",
+        course: "DAE (ELECTRICAL)",
+        total: "3450",
+        obt: "2800",
+        per: "81.15%",
+        grade: "A-1"
+    },
+    {
+        roll: "90607",
+        year: "2024",
+        name: "FAHAD",
+        father: "MUNEER AHMED",
+        reg: "BSTE/TEC/A261-9744",
+        course: "DAE (ELECTRICAL)",
+        total: "3450",
+        obt: "2800",
+        per: "81.15%",
+        grade: "A-1"
+    },
+    {
+        roll: "90608",
+        year: "2023",
+        name: "SARANG ALI",
+        father: "SONO KHAN",
+        reg: "BSTE/TEC/A261-9745",
+        course: "DAE (ELECTRICAL)",
+        total: "3450",
+        obt: "2800",
+        per: "81.15%",
+        grade: "A-1"
+    },
+    {
+    
+    {
+        roll: "90606",
+        year: "2026",
+        name: "MOHSIN KHAN",
+        father: "AHMED KHAN",
+        reg: "BSTE/TEC/A261-9800",
+        course: "DAE (ELECTRICAL)",
+        total: "3450",
+        obt: "2800",
+        per: "81.15%",
+        grade: "A-1"
+    },
+    {
+        roll: "90606",
+        year: "2026",
+        name: "MOHSIN KHAN",
+        father: "AHMED KHAN",
+        reg: "BSTE/TEC/A261-9800",
+        course: "DAE (ELECTRICAL)",
+        total: "3450",
+        obt: "2800",
+        per: "81.15%",
+        grade: "A-1"
+    }
+    // Is tarah aap jitni chahen entries niche add karte jayen
+];
